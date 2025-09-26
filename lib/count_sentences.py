2caa18c2beed8ba3cd9f8afc378241b8c8f22b19
Ian Njuguna <ian.njuguna@student.moringaschool.com>
2025-09-26 15:49:17 +0300

    sount sentenses

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value   # use setter
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        if isinstance(val, str):
+            self._value = val
+        else:
+            print("The value must be a string.")
+            self._value = ""
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        import re
+        sentences = re.split(r'[.!?]+(?:\s|$)', self.value)
+        sentences = [s for s in sentences if s.strip()]
+        return len(sentences)
+
+    def __str__(self):
+        return self.value
